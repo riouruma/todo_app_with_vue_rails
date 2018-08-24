327f32ccc6026fe4778d3a36cef429e8d48fc297
riouruma <rio.uruma@gmail.com>
2018-08-24 16:28:06 +0900

    finish todo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/about', to: 'home#index'
+  get '/contact', to: 'home#index'
+
+  namespace :api, format: 'json' do
+    resources :tasks, only: %w(index create update)
+  end
 end
